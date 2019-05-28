43c071e5e44109190a06e50a96903080c7356620
dawid <dawidgardzi@onet.pl>
2019-05-29 01:19:54 +0200

    first commit

---
+++ precache-manifest.7dce00c7e12c06425a905d093dd400d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55a36b866de85c8248a7",
+    "url": "/static/js/main.a4661e68.chunk.js"
+  },
+  {
+    "revision": "f11198bf34f5fa66168b",
+    "url": "/static/js/2.7eae1618.chunk.js"
+  },
+  {
+    "revision": "55a36b866de85c8248a7",
+    "url": "/static/css/main.4a361e8a.chunk.css"
+  },
+  {
+    "revision": "f11198bf34f5fa66168b",
+    "url": "/static/css/2.6a1dec0e.chunk.css"
+  },
+  {
+    "revision": "714e270f00dcb493cf7c182ceaf64276",
+    "url": "/index.html"
+  }
+];
